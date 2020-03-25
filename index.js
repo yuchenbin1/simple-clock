f5737f3786028343b8c09a60f0cf24509ce902c9
yuchenbin1 <2452846575@qq.com>
2020-03-25 19:47:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+window.setInterval(()=>{
+	
+var now=new Date();
+var h=now.getHours();
+var m=now.getMinutes();
+var s=now.getSeconds();
+
+const hourEl=document.querySelector(".hour");
+const minuteEl=document.querySelector(".minute");
+const secondEl=document.querySelector(".second");
+
+if(h<=12){
+	var angleH=(h+m/60+s/3600)*30;
+}else{
+	var angleH=((h+m/60+s/3600)-12)*30;
+}
+
+	var angleM=(m+s/60)*6;
+	var angleS=s*6;
+	hourEl.style.transform="rotate("+angleH+"deg)";//为什么一定要加+好呢
+	minuteEl.style.transform="rotate("+angleM+"deg)";
+	secondEl.style.transform="rotate("+angleS+"deg)";
+},1000);
